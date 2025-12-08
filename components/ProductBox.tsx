8318a79c7123d23f9038509c13d1704f4e63b79e
ferico <huangferico@gmail.com>
2025-12-08 21:17:34 +0700

    add link to detail page

--- components/ProductBox.tsx
+++ components/ProductBox.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'expo-router';
 import { Image, Text, View } from 'react-native';
 
 type ProductBoxProps = {
@@ -10,11 +11,15 @@ type ProductBoxProps = {
 export default function ProductBox({ id, image, name, price }: ProductBoxProps) {
   return (
     <View className="w-[48%] mb-[25]">
-      <Image source={{ uri: image }} className="w-full h-[200] mb-[15]" resizeMode="cover" />
-      <Text className="font-outfit h-[40] overflow-hidden" numberOfLines={2} ellipsizeMode="tail">
-        {name}
-      </Text>
-      <Text className="font-outfit mt-[5]">Rp. {price}</Text>
+      <Link href={{ pathname: '/product/[id]', params: { id } }}>
+        <Image source={{ uri: image }} className="w-full h-[200] mb-[15]" resizeMode="cover" />
+        <View>
+          <Text className="font-outfit h-[40] overflow-hidden" numberOfLines={2} ellipsizeMode="tail">
+            {name}
+          </Text>
+          <Text className="font-outfit mt-[5]">Rp. {price}</Text>
+        </View>
+      </Link>
     </View>
   );
 }
